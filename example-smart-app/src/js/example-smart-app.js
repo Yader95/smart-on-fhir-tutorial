d9d29dbe7c638ce80e16f911ae07ed3df886ce1f
Yader <yguadamuz@gmail.com>
2025-08-27 12:23:46 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -20,6 +20,7 @@
                               //'http://loinc.org|8480-6', //Systolic Blood Pressure
                               'http://loinc.org|2085-9', //HDL
                               'http://loinc.org|2089-1', //LDL
+                              'http://loinc.org|8310-5', //Temp
                               'http://loinc.org|85354-9'] //Blood Pressure
                       }
                     }
@@ -44,6 +45,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var ldl = byCodes('8310-5');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -62,7 +64,8 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
-
+          p.temp = getQuantityValueAndUnit(temp[0]);
+        
           ret.resolve(p);
         });
       } else {
@@ -86,6 +89,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      temp: {value: ''},
     };
   }
 
@@ -129,6 +133,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+     $('#temp').html(p.temp);
   };
 
 })(window);
