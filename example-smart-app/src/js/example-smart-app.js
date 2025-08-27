5348752a7a5d7074ea4540ca0dc868b12cfa8db5
Yader <yguadamuz@gmail.com>
2025-08-27 12:31:09 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -45,7 +45,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-          var ldl = byCodes('8310-5');
+          var temp = byCodes('8310-5');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -89,7 +89,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
-      temp: {value: ''},
+      temp: {value: ''}
     };
   }
 
